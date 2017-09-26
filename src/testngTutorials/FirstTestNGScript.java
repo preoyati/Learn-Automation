0ed16ada3c85000860552d1ca313edfa57597a7e
Preoyati Khan <pkhan1@kent.edu>
2017-09-26 02:25:44 -0400

    First commit

---
+++ src/testngTutorials/FirstTestNGScript.java
@@ -0,0 +1,27 @@
+package testngTutorials;
+
+import org.testng.annotations.Test;
+
+public class FirstTestNGScript {
+	
+	@Test(priority = 1, description = "this test is for login")
+	public void loginApplication()
+	{
+		System.out.println("First Test nu testNG");
+		
+	}
+	@Test(priority = 2)
+	public void selectItems()
+	{
+		System.out.println("select Items");
+		
+	}
+	@Test(priority = 3)
+	public void checkOut()
+	{
+		System.out.println("scheckout");
+		
+	}
+
+
+}
